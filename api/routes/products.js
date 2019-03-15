9b3244eaa8772754a145afd016ffdb1376b6041b
paulocesarcit <paulocesarcit@hotmail.com>
2019-03-14 23:28:07 -0300

    db corrections

--- api/routes/products.js
+++ api/routes/products.js
@@ -9,7 +9,13 @@ router.get('/', (req, res, next) => {
         .exec()
         .then(docs => {
             console.log(docs)
+            // if (docs.length >= 0) {
             res.status(200).json(docs)
+            // } else {
+            //     res.status(404).json({
+            //         message: 'No entries found'
+            //     })
+            // }
         })
         .catch(err => {
             console.log(err)
@@ -43,9 +49,9 @@ router.get('/:productId', (req, res, next) => {
     Product.findById(id)
         .exec()
         .then(doc => {
-            console.log(doc)
+            console.log('From database', doc)
             if (doc) {
-                res.status(200).json('From database', doc)
+                res.status(200).json(doc)
             } else {
                 res.status(404).json({message: 'No valid entry found for provided ID'})
             }
@@ -57,15 +63,34 @@ router.get('/:productId', (req, res, next) => {
 })
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated product!'
-    })
+    const id = req.params.productId
+    const updateOps = {}
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
+    }
+    Product.update({_id: id}, {$set: updateOps})
+        .exec()
+        .then(result => {
+            console.log(result)
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: err})
+        })
 })
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
-    })
+    const id = req.params.productId
+    Product.remove({_id: id})
+        .exec()
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: err})
+        })
 })
 
 module.exports = router
